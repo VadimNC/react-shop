80994afd3b0d0813c2f3ba040ba9d6f2e8636244
VadimNC <rezervv@ukr.net>
2024-02-05 07:59:37 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import AppRoutes from "../Routes/Routes";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Sidebar from "../Sidebar/Sidebar";
+import {getCategories} from "../../features/categories/categoriesSlice";
+import {getProducts} from "../../features/products/productsSlice";
+import UserForm from "../User/UserForm";
+
+export default function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCategories());
+        dispatch(getProducts());
+    }, [dispatch]);
+
+    return (
+        <div className="app">
+            <Header/>
+            <UserForm/>
+            <div className="container">
+                <Sidebar/>
+                <AppRoutes/>
+            </div>
+            <Footer/>
+        </div>
+    );
+};
---
+++ src/components/Categories/SingleCategory.jsx
@@ -0,0 +1,11 @@
+import Category from "./Category";
+import Poster from "../Poster/Poster";
+
+export default function SingleCategory() {
+    return (
+        <>
+            <Poster/>
+            <Category/>
+        </>
+    );
+};
